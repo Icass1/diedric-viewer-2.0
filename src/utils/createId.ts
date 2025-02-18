0956f21b8612b0dd03793211114d71cbe8681bff
icassCodeServer <i.cassdolopez@gmail.com>
2025-02-18 11:19:54 +0100

    Removed numbers from makeId

--- src/utils/createId.ts
+++ src/utils/createId.ts
@@ -1,7 +1,7 @@
 export function makeid(length: number) {
     let result = "";
     const characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < length) {
