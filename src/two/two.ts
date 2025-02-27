50b8a5d53fba574c4b0cafbf98920314d03d17f2
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-02-27 10:16:04 +0100

    Removed two folder to convert it into a submodule

--- src/two/two.ts
+++
@@ -1,7 +0,0 @@
-export { Canvas } from "./canvas";
-export { Line } from "./line";
-export { Point } from "./point";
-export { Label } from "./label";
-export { Circle } from "./circle";
-export { Ellipse } from "./ellipse";
-
